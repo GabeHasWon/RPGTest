e8c9bd5e88cc324fa4e3dc28aa5041881a4d5bd3
GabeHasWon <gabriel@guevarameyer.com>
2017-09-12 18:25:29 -0500

    Add files via upload

---
+++ Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGTest
+{
+    public static class Extensions
+    {
+        public static void AddMultiple<T>(this List<T> list, T one, T two)
+        {
+            list.Add(one);
+            list.Add(two);
+        }
+
+        public static void AddMultiple<T>(this List<T> list, T one, T two, T three)
+        {
+            list.Add(one);
+            list.Add(two);
+            list.Add(three);
+        }
+
+        public static void AddMultiple<T>(this List<T> list, T one, T two, T three, T four)
+        {
+            list.Add(one);
+            list.Add(two);
+            list.Add(three);
+            list.Add(four);
+        }
+    }
+}
